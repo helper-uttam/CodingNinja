f34192cd4e5e2d894bc1f2d5e1025b506a0ffdf6
unknown <bajiraouttamsinha@gmail.com>
2022-01-13 19:15:44 +0530

    fixing color issue and adding dynamic skip button

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -4,6 +4,7 @@ import classes from './Card.module.css';
 const Card = (props) => {
     var [counter, setCounter] = useState(0);
     var lastId = null;
+    var [loading, setLoading] = useState(false);
 
     const handleCounter = () => {
         setCounter(++counter);
@@ -23,12 +24,18 @@ const Card = (props) => {
             let audio = new Audio("/assets/oh_no.mp3");
             audio.play()
         }
+        setLoading(true);
+        setTimeout(() => {
+            handleCounter();
+            document.getElementById(lastId).style.color="";
+            setLoading(false);
+        }, 4000);
     }
     
     // console.log(props.a[0].answer_e );
     return(<div id="question_area" className={classes.card_body}>
         <h1>Question</h1>
-        <h4 className={classes.que}>{props.q[counter]}</h4>
+        <h4 className={classes.que}>{counter+1}.  {props.q[counter]}</h4>
         <h4>Options:</h4>
         <div  className={classes.options}>
             <div id="1" className={classes.one} onClick={checkAns} >
@@ -55,7 +62,7 @@ const Card = (props) => {
                 {props.a[counter].answer_e}
                 </div>
             }
-            <button className={classes.next} onClick={handleCounter}>Next</button>
+            <button className={classes.next} onClick={handleCounter}>{loading ? "Please wait ..." : "Skip"}</button>
         </div>
     </div>);
 }
