2e8d449fb9fe2851581a65250502e30ebb643494
unknown <bajiraouttamsinha@gmail.com>
2022-02-27 21:32:01 +0530

    fixing responsiveness

--- src/components/pages/Signin.jsx
+++ src/components/pages/Signin.jsx
@@ -1,10 +1,11 @@
 import classes from "./Signin.module.css";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useHistory } from "react-router-dom";
 import Sawo from 'sawo';
 
 
 const Login = () => {
+    const [width, setWidth] = useState();
     const history = useHistory();
     useEffect(() => {
         var config = {
@@ -18,18 +19,18 @@ const Login = () => {
             onSuccess: payload => {
                 console.log(payload)
                 // you can use this payload for your purpose
-
                 localStorage.setItem('email','users.email');
                 window.location.reload();
             },
         }
         let sawo = new Sawo(config)
         sawo.showForm()
-        
+        console.log(window.innerWidth);
+        setWidth(window.innerWidth);
     }, [])
     
     return  <div className={classes.sawoContainer}>
-                <div className={classes.sawo} id="sawo-container" style={{height:"400px", width:"500px"}}></div>
+                <div className={classes.sawo} id="sawo-container" style={width < 1180 ? {marginTop: "10px", height:"100%", width:"100%"}: {height:"50vh", width:"50vw"}}></div>
             </div>
 }
 export default Login;
