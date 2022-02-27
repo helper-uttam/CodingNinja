c453802cdc391e52fe64f35a6f17618d0ecfb092
unknown <bajiraouttamsinha@gmail.com>
2022-02-27 08:37:04 +0530

    adding firebase deployment

--- src/components/pages/Signin.jsx
+++ src/components/pages/Signin.jsx
@@ -13,7 +13,7 @@ const Login = () => {
             // can be one of 'email' or 'phone_number_sms'
             identifierType: 'email',
             // Add the API key copied from 5th step
-            apiKey: REACT_APP_SAWO_API_KEY,
+            apiKey: process.env.REACT_APP_SAWO_API_KEY,
             // Add a callback here to handle the payload sent by sdk
             onSuccess: payload => {
                 console.log(payload)
